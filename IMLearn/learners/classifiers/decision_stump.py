7b37e32676a180626de70fd0b288d34bbebca3a8
hila ziv <hila.ziv@mail.huji.ac.il>
2022-06-04 09:51:38 +0300

    ex4 done

--- IMLearn/learners/classifiers/decision_stump.py
+++ IMLearn/learners/classifiers/decision_stump.py
@@ -3,6 +3,7 @@ from typing import Tuple, NoReturn
 from ...base import BaseEstimator
 import numpy as np
 from itertools import product
+from ...metrics import misclassification_error
 
 
 class DecisionStump(BaseEstimator):
@@ -39,7 +40,17 @@ class DecisionStump(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        raise NotImplementedError()
+        signs = {-1, 1}
+        minimal_loss = 2  # can be every value > 1
+        for sign in signs:
+            loss_per_feature = np.apply_along_axis(self._find_threshold, 0, X, y, sign).T
+            minimal_loss_index = np.argmin(loss_per_feature[:, 1])
+            if minimal_loss >= loss_per_feature[minimal_loss_index][1]:
+                minimal_loss = loss_per_feature[minimal_loss_index][1]
+                self.j_ = minimal_loss_index
+                self.threshold_ = loss_per_feature[minimal_loss_index][0]
+                self.sign_ = sign
+        self.fitted_ = True
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -63,7 +74,10 @@ class DecisionStump(BaseEstimator):
         Feature values strictly below threshold are predicted as `-sign` whereas values which equal
         to or above the threshold are predicted as `sign`
         """
-        raise NotImplementedError()
+        y_pred = np.empty(len(X))
+        y_pred[X[:, self.j_] < self.threshold_] = -self.sign_
+        y_pred[X[:, self.j_] >= self.threshold_] = self.sign_
+        return y_pred
 
     def _find_threshold(self, values: np.ndarray, labels: np.ndarray, sign: int) -> Tuple[float, float]:
         """
@@ -95,7 +109,17 @@ class DecisionStump(BaseEstimator):
         For every tested threshold, values strictly below threshold are predicted as `-sign` whereas values
         which equal to or above the threshold are predicted as `sign`
         """
-        raise NotImplementedError()
+        sorted_indices = np.argsort(values)
+        values = values[sorted_indices]
+        labels = labels[sorted_indices]
+        n_samples = values.shape[0]
+        y_pred = np.zeros(n_samples)
+        losses = np.zeros(n_samples)
+        for i in range(n_samples):
+            y_pred[:i] = np.abs(labels[:i]) * (-sign)
+            y_pred[i:] = np.abs(labels[i:]) * sign
+            losses[i] = np.abs(labels - y_pred).sum() / n_samples
+        return values[np.argmin(losses)], losses.min()
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -114,4 +138,4 @@ class DecisionStump(BaseEstimator):
         loss : float
             Performance under missclassification loss function
         """
-        raise NotImplementedError()
+        return misclassification_error(y, self._predict(X))
--- IMLearn/metalearners/adaboost.py
+++ IMLearn/metalearners/adaboost.py
@@ -1,5 +1,5 @@
 import numpy as np
-from ...base import BaseEstimator
+from ..base import BaseEstimator
 from typing import Callable, NoReturn
 
 
@@ -48,7 +48,22 @@ class AdaBoost(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        raise NotImplementedError()
+        n_samples = len(y)
+        self.models_ = np.empty(self.iterations_, dtype=BaseEstimator)
+        self.weights_ = np.empty(self.iterations_)
+        self.D_ = np.empty((self.iterations_ + 1, n_samples))
+        self.D_[0] = np.ones(n_samples) / (n_samples)
+
+        for t in range(self.iterations_):
+            wl = self.wl_()
+            wl.fit(X, y * self.D_[t])
+            y_pred = wl.predict(X)
+            error = np.sum(self.D_[t][y != y_pred])
+            w = 0.5 * np.log((1 / error) - 1)
+            self.D_[t + 1] = self.D_[t] * np.exp(-y * (w * y_pred))
+            self.D_[t + 1] /= np.sum(self.D_[t + 1])
+            self.models_[t] = wl
+            self.weights_[t] = w
 
     def _predict(self, X):
         """
@@ -64,7 +79,7 @@ class AdaBoost(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        raise NotImplementedError()
+        return self.partial_predict(X, self.iterations_)
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -83,7 +98,7 @@ class AdaBoost(BaseEstimator):
         loss : float
             Performance under missclassification loss function
         """
-        raise NotImplementedError()
+        return self.partial_loss(X, y, self.iterations_)
 
     def partial_predict(self, X: np.ndarray, T: int) -> np.ndarray:
         """
@@ -102,7 +117,7 @@ class AdaBoost(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        raise NotImplementedError()
+        return np.sign(np.sum(np.array([self.weights_[t] * self.models_[t].predict(X) for t in range(T)]), axis=0))
 
     def partial_loss(self, X: np.ndarray, y: np.ndarray, T: int) -> float:
         """
@@ -124,4 +139,5 @@ class AdaBoost(BaseEstimator):
         loss : float
             Performance under missclassification loss function
         """
-        raise NotImplementedError()
+        from IMLearn.metrics.loss_functions import misclassification_error
+        return misclassification_error(y, self.partial_predict(X, T))
